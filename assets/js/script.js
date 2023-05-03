859fd4e42ddfb81e8a217718c2e7e47308787e74
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-05-04 06:49:31 +0800

    Progress: Revision UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ const inputCalc = document.querySelector('.calculator-field');
 const dataNumber = document.querySelectorAll("[data-number]");
 const dataOperator = document.querySelectorAll("[data-operator]");
 
-buttonCalc.forEach(e => {
-    e.addEventListener('click', function(i) {
-        let dataNumber = i.target.getAttribute("data-number");
-        let dataOperator = i.target.getAttribute("data-operator");
-        let inputData = inputCalc.innerHTML;
-        inputCalc.innerHTML = inputData + dataNumber;
-    });
-});
+// buttonCalc.forEach(e => {
+//     e.addEventListener('click', function(i) {
+//         let dataNumber = i.target.getAttribute("data-number");
+//         let dataOperator = i.target.getAttribute("data-operator");
+//         let inputData = inputCalc.innerHTML;
+//         inputCalc.innerHTML = inputData + dataNumber;
+//     });
+// });
