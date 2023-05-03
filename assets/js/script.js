4d6b3efa1660152caf1d5c403aa834e2dc2c8952
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-05-03 23:06:50 +0800

    Progress: Calculator UI

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const buttonCalc = document.querySelectorAll('button');
+const inputCalc = document.querySelector('.calculator-field');
+const dataNumber = document.querySelectorAll("[data-number]");
+const dataOperator = document.querySelectorAll("[data-operator]");
+
+buttonCalc.forEach(e => {
+    e.addEventListener('click', function(i) {
+        let dataNumber = i.target.getAttribute("data-number");
+        let dataOperator = i.target.getAttribute("data-operator");
+        let inputData = inputCalc.innerHTML;
+        inputCalc.innerHTML = inputData + dataNumber;
+    });
+});
